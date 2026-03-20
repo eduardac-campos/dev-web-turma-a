6b76ccc9f6ec2e9e1d301301e0c2eff44f778078
Eduarda Carvalho <eduarda.ccampos@sempreceub.com>
2026-03-19 21:29:15 -0300

    Instalando Vue

---
+++ Eduarda Carvalho Campos/Aulas/vue_cli/src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Bem indo: {{ name }}Olá mundo o <b>VUE com CLI</b>!</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeComponent',
+            data() {
+                return {
+                name: 'Eduarda'
+        }
+    }
+}
+
+</script>
