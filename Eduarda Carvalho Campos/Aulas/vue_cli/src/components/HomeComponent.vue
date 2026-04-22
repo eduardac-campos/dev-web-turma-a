acdcb8c8331d726a2c42db552b4760fd2bb9cc67
Eduarda Carvalho Campos <eduarda.ccampos@sempreceub.com>
2026-04-22 20:51:26 -0300

    Sincronizando o repositorio

--- Eduarda Carvalho Campos/Aulas/vue_cli/src/components/HomeComponent.vue
+++ Eduarda Carvalho Campos/Aulas/vue_cli/src/components/HomeComponent.vue
@@ -1,19 +1,18 @@
 <template>
-    <div>
-        <footer id="footer">
-            <p>Bem indo: {{ name }}Olá mundo o <b>VUE com CLI</b>!</p>
-        </footer>
-    </div>
+  <div>
+    <footer id="footer">
+      <p>Ben Vindo: {{ nome }} Olá Mundo do <b>VUE com CLI</b></p>
+    </footer>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'HomeComponent',
-            data() {
-                return {
-                name: 'Eduarda'
-        }
-    }
-}
-
+export default {
+  name: "HomeComponent",
+  data() {
+    return {
+      nome: "Fernando",
+    };
+  },
+};
 </script>
